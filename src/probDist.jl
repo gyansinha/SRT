6bf94a8fafe76cf75f0e162705e539131cbe3377
Gyan Sinha <gsinha@godolphincapital.com>
2025-02-24 10:16:56 -0500

    added tranche_valuation function

--- src/probDist.jl
+++ src/probDist.jl
@@ -3,17 +3,16 @@ using QuadGK
 using Dates
 using Distributed
 using BenchmarkTools
+using OrderedCollections
 
 
 """
 
-    function pCond(M::Float64, N::Int64, t::Float64, a::Float64, h::Float64)
+    function pCond!(p::Matrix{Float64}, M::Float64, N::Int64, a::Float64, q::Float64)
 
 """
-function pCond!(p::Matrix{Float64}, M::Float64, N::Int64, t::Float64, a::Float64, h::Float64)
+function pCond!(p::Matrix{Float64}, M::Float64, N::Int64, a::Float64, q::Float64)
                                
-  q = 1.0 - exp(-h * t)
-
   d = Normal()
   x = quantile(d, q)
   q_m = cdf(d, (x - a * M)/sqrt(1 - a * a))
@@ -36,9 +35,9 @@ end
 """
     function f(x::Float64, N::Int64, t::Float64, a::Float64, h::FLoat64, l::Int64, p::Matrix{Float64})
 """
-function f(x::Float64, N::Int64, t::Float64, a::Float64, h::Float64, l::Int64, p::Matrix{Float64})
+function f(x::Float64, N::Int64, a::Float64, q::Float64, l::Int64, p::Matrix{Float64})
 
-    pCond!(p, x, N, t, a, h)
+    pCond!(p, x, N, a, q)
     y = p[N, l] * 1.0/√(2.0 * π) * exp(-0.5*x^2)
 
     return y 
@@ -46,25 +45,26 @@ end
 
 
 """
-    function vrad_integration(N::Int64, t::Float64, a::Float64, h::Float64, l::Int64, buf)
+    function vrad_integration(N::Int64, a::Float64, q::Float64, l::Int64, 
+        buf)
 """
-function vrad_integration(N::Int64, t::Float64, a::Float64, h::Float64, l::Int64, p::Matrix{Float64}, 
-    buf)
+function vrad_integration(N::Int64, a::Float64, q::Float64, l::Int64, p::Matrix{Float64}, buf)
 
-    val = quadgk(x -> f(x, N, t, a, h, l, p), -Inf, Inf; segbuf=buf)[1]
+    val = quadgk(x -> f(x, N, a, q, l, p), -Inf, Inf; segbuf=buf)[1]
     return val
 end
 
 
 """
-    function probDist(N::Int64, t::Float64, a::Float64, h::Float64)
+    function probDist!(N::Int64, a::Float64, q::Float64, p::Matrix{Float64},
+        p_uncond::Vector{Float64}, buf)
 
 """
-function probDist!(N::Int64, t::Float64, a::Float64, h::Float64, p::Matrix{Float64},
+function probDist!(N::Int64, a::Float64, q::Float64, p::Matrix{Float64},
     p_uncond::Vector{Float64}, buf)
 
     for l in 1:N+1
-        p_uncond[l] = vrad_integration(N, t, a, h, l, p, buf)
+        p_uncond[l] = vrad_integration(N, a, q, l, p, buf)
     end
     
     return p_uncond
@@ -74,8 +74,8 @@ end
 """
     function create_schedule(A::Float64, T::Int64, cpr::Float64)
 """
-function create_schedule(A::Float64, T::Int64, cpr::Float64)
-    smm = 1 - (1-cpr/100)^(1/12)
+function create_schedule(A::Float64, T::Int64, cpr::Float64; period::Int64=12)
+    smm = 1 - (1-cpr/100)^(1/period)
     B = zeros(T)
     B[1] = A
     for t in 2:T
@@ -85,63 +85,72 @@ function create_schedule(A::Float64, T::Int64, cpr::Float64)
 end
 
 
-N = 100
-A = 1.0e+07
-R = 0.40
+"""
+    function tranche_valuation(attach::Float64, detach::Float64, ρ::Float64, 
+        single_name_spread::Float64, tranche_spread::Float64, first_pay_date::Date, last_pay_date::Date; 
+        N::Int64=100, A::Float64=10.0e+06, R::Float64=0.40,  periods::Int64=12, r_f::Float64=0.0425)
 
-first_pay_date = Date("2025-04-25")
-last_pay_date = Date("2032-02-25")
-pmt_dates = collect(first_pay_date:Month(1):last_pay_date)
-num_periods = length(pmt_dates)
+"""
+function tranche_valuation(attach::Float64, detach::Float64, ρ::Float64, 
+    single_name_spread::Float64, tranche_spread::Float64, first_pay_date::Date, last_pay_date::Date; 
+    N::Int64=100, A::Float64=10.0e+06, R::Float64=0.40,  periods::Int64=12, r_f::Float64=0.0425)
 
-ρ = 0.90
-a = sqrt(ρ)
-single_name_spread = 98/10_000
-h = single_name_spread/(1-R)
-dt = (3/12)
-r_f = 0.0425
+    pmt_dates = collect(first_pay_date:Month(1):last_pay_date)
+    T = length(pmt_dates)
 
-attach = 0.0475
-detach = 0.1250
-H = detach * (A * N)
-L = attach * (A * N)
+    a = sqrt(ρ)
+    h = single_name_spread/(1-R)
+    dt = 1/periods
 
-d = exp.(-r_f * dt .* (1:num_periods))
+    H = detach * (A * N)
+    L = attach * (A * N)
 
-el = zeros(num_periods)
-ufee = zeros(num_periods)
+    d = exp.(-r_f * dt .* (1:T))
 
-buf = alloc_segbuf()
-p = zeros(Float64, N+1, N+1)
-p_uncond = zeros(Float64, N+1)
+    el = zeros(T)
+    ufee = zeros(T)
 
-p_dist = zeros(N+1, num_periods)
+    buf = alloc_segbuf()
+    p = zeros(Float64, N+1, N+1)
+    p_uncond = zeros(Float64, N+1)
 
-B = create_schedule(A, num_periods, 25.0)
+    p_dist = zeros(N+1, T)
 
-for i in 1:num_periods
-    tper = i * dt
-    p_dist[:, i] = probDist!(N, tper, a, h, p, p_uncond, buf)
-    per_loan_loss = B[i] * (1 - R)
-    loss = 0.0
-    for l in 1:N+1
-        loss += p_dist[l, i] * max(min((l-1) * per_loan_loss, H) - L, 0)
+    B = create_schedule(A, T, 25.0)
+
+    for i in 1:T
+        t = i * dt
+        q = 1.0 - exp(-h * t)
+        p_dist[:, i] = probDist!(N, a, q, p, p_uncond, buf)
+        per_loan_loss = B[i] * (1 - R)
+        loss = 0.0
+        for l in 1:N+1
+            loss += p_dist[l, i] * max(min((l-1) * per_loan_loss, H) - L, 0)
+        end
+        el[i] = loss
+        ufee[i] = (d[i] * dt * ((H - L) - el[i]))
+    
+        # println("i: $i, t: $t, el: $(el[i]), ufee: $(ufee[i])")
     end
-    el[i] = loss
-    ufee[i] = (d[i] * dt * ((H - L) - el[i]))
+    
+    contingent = zeros(T)
+    contingent[1] = d[1] * el[1] 
+    for i in 2:T 
+        contingent[i] = d[i] * (el[i] - el[i-1])
+    end
+    total_contingent = sum(contingent)
+    spar = total_contingent / sum(ufee)
+    println("Par Spread on $(100*attach)% to $(100*detach)% tranche: $(10_000*spar) bps")
+    
+    fee = tranche_spread * sum(ufee)
+    mtm = fee - total_contingent
 
-    println("i: $i, tper: $tper, el: $(el[i]), ufee: $(ufee[i])")
+    result = OrderedDict("Par Spread" => spar * 10_000, "MtM" => mtm)
+    return result
 end
 
-contingent = zeros(num_periods)
-contingent[1] = d[1] * el[1] 
-for i in 2:num_periods 
-    contingent[i] = d[i] * (el[i] - el[i-1])
-end
-total_contingent = sum(contingent)
-spar = total_contingent / sum(ufee)
-println("Par Spread on $(100*attach)% to $(100*detach)% tranche: $(10_000*spar) bps")
 
-s = 315/10000
-fee = s * sum(ufee)
-mtm = fee - total_contingent
+
+
+
+
